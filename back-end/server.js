f01024e66aad789f84cc2b0e76b8971a2393dd41
Ubuntu <ubuntu@ip-172-31-93-240.ec2.internal>
2022-09-07 14:26:29 +0000

    Setup dasar express

---
+++ back-end/server.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+app.use('/', (req, res) => {
+    return res.send('Selamat datang di persona...')
+})
+
+http.createServer(app).listen(process.env.PORT, () => {
+    console.log(`Server dimulai di port ${process.env.PORT}`)
+})
