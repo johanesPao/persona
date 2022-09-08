b903cdae53ad85b4b69dc9e4b1bb850d90356ac8
Johanes Gregorius Indra Pradana Pao <johanes.pao@gmail.com>
2022-09-08 09:30:52 +0000

    Fix express.Router() tidak diassign pada app.use() di app.js (II)

--- back-end/sumber/app.js
+++ back-end/sumber/app.js
@@ -3,9 +3,9 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
-const api = require('./rute/api')
+// const api = require('./rute/api')
 require('dotenv').config()
-const VERSI_API = process.env.VERSI_API
+// const VERSI_API = process.env.VERSI_API
 
 const app = express()
 
@@ -17,7 +17,7 @@ app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'publik')))
 
-app.use(`/${VERSI_API}`, api)
+// app.use(`/${VERSI_API}`, api)
 
 app.get('/*', (req, res) => {
     res
