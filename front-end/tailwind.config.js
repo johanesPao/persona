14ae1454b085aee403143872f20e0ca9972a5209
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2022-09-16 17:02:42 +0700

    Pengaturan tailwindcss di front-end

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
-  theme: {},
+  content: ['./src/**/*.{js,jsx}', './public/index.html'],
+  theme: {
+    colors: {
+      dark: '#16213E',
+      navy: '#0F3460',
+      purple: '#533483',
+      red: '#E94560',
+      white: '#FFFFFF',
+    },
+  },
   plugins: [],
 };
