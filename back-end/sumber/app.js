05e8d6ce55ccfced6996e464bf7429f8621a0ae3
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2022-09-17 00:19:05 +0700

    Perubahan kebijakan CORS di back-end API

--- back-end/sumber/app.js
+++ back-end/sumber/app.js
@@ -1,30 +1,31 @@
-const path = require('path')
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const api = require('./rute/api')
-require('dotenv').config()
-const VERSI_API = process.env.VERSI_API
+const api = require('./rute/api');
+require('dotenv').config();
+const VERSI_API = process.env.VERSI_API;
 
-const app = express()
+const app = express();
 
-const ruteTulisan = require('./rute/tulisan/tulisan.rute')
+const ruteTulisan = require('./rute/tulisan/tulisan.rute');
 
-app.use(cors({
-    origin: 'http://localhost:3000' // front-end
-}))
-app.use(morgan('combined'))
+app.use(
+  cors({
+    // bisa menerima rekues dari berbagai origin, autentikasi dan autorisasi akan dilakukan di level api method
+    origin: '*',
+  }),
+);
+app.use(morgan('combined'));
 
-app.use(express.json())
-app.use(express.static(path.join(__dirname, '..', 'publik')))
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'publik')));
 
-app.use(`/${VERSI_API}`, api)
+app.use(`/${VERSI_API}`, api);
 
 app.get('/*', (req, res) => {
-    res
-        .status(200)
-        .sendFile(path.join(__dirname, '..', 'publik', 'index.html'))
-})
+  res.status(200).sendFile(path.join(__dirname, '..', 'publik', 'index.html'));
+});
 
-module.exports = app
+module.exports = app;
